658bd74f5cf6c4c51c98d674f77f77f26b21170f
Judson Flamm <superlama01@gmail.com>
2020-10-12 00:20:38 +0000

    implemented client side rendering of new creature format

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -38,20 +38,40 @@ ws.on('gen', function (msg) {
 	if (msg == null) return;
 	creatureContainer.innerHTML = '';
 	for (var i = 0; i < msg.length; i++) {
+		const cells = msg[i].activeCells;
+		const boundaries = [[0, 0], [0, 0]];
+		cells.forEach(cell => {
+			cell.forEach((coord, j) => {
+				if (coord < boundaries[j][0]) boundaries[j][0] = coord;
+				else if (coord > boundaries[j][1]) boundaries[j][1] = coord;
+			});
+		});
+		const size = boundaries.map(boundary => boundary[1] - boundary[0] + 1);
+		const body = [];
+		for (let j = 0; j < size[0]; j++) {
+			const row = [];
+			for (let k = 0; k < size[1]; k++) {
+				row.push(false);
+			}
+			body.push(row);
+		}
+		cells.forEach(cell => {
+			body[cell[0] - boundaries[0][0]][cell[1] - boundaries[1][0]] = true;
+		});
 		var c = document.createElement('canvas');
 		c.classList.add('creature');
 		c = creatureContainer.appendChild(c);
 		var ctx = c.getContext('2d');
 		var rect = window.getComputedStyle(c);
-		var size = [c.clientWidth * window.devicePixelRatio, c.clientHeight * window.devicePixelRatio];
-		c.width = size[0];
-		c.height = size[1];
+		var cSize = [c.clientWidth * window.devicePixelRatio, c.clientHeight * window.devicePixelRatio];
+		c.width = cSize[0];
+		c.height = cSize[1];
 		ctx.fillStyle = 'black';
 		ctx.strokeStyle = 'white';
-		for (var j = 0; j < msg[i].body.length; j++) {
-			for (var k = 0; k < msg[i].body[j].length; k++) {
-				if (msg[i].body[j][k]) {
-					var offset = [size[0] / msg[i].body.length, size[1] / msg[i].body[j].length];
+		for (var j = 0; j < body.length; j++) {
+			for (var k = 0; k < body[j].length; k++) {
+				if (body[j][k]) {
+					var offset = [cSize[0] / body.length, cSize[1] / body[j].length];
 					ctx.lineWidth = Math.min(...offset) / 10;
 					var coord = [j * offset[0], k * offset[1]];
 					ctx.fillRect(...coord, ...offset);
